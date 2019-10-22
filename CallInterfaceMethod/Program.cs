be0c16dfc3345219d27f5c40de9b37fefae12975
mig-25 <kd3180@hotmail.com>
2019-10-22 22:46:08 +0200

    Add project files.

---
+++ CallInterfaceMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CallInterfaceMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
